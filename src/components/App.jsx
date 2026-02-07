5a7a3d2303f8154ee7dc02454a9c6cc09886e35a
Porter <pdhakes25@gmail.com>
2026-02-07 16:08:09 -0700

    initial set up WIP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,24 @@
-// create your App component here
+import react from "react";
+import useState from "react";
+import useEffect from "react";
+
+function App() {
+    const [image, setImage] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        console.log("image on render")
+    },[])
+
+    function callImage() {
+        fetch()
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default App
