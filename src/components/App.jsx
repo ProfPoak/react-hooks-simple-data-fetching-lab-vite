e70c7d7df4616fbd68d87c7441de52f3afd06786
Porter <pdhakes25@gmail.com>
2026-02-07 16:30:44 -0700

    First Submission

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,33 @@
-import react from "react";
-import useState from "react";
-import useEffect from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
     const [image, setImage] = useState("")
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        console.log("image on render")
+        callImage()
     },[])
 
     function callImage() {
-        fetch()
+        setLoading(true)
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(response => response.json())
+            .then(data => {
+                setImage(data.message)
+                setLoading(false)
+            })
+            .catch(error => {
+                console.error("error:", error)
+                setLoading(false)
+            });
+        
     }
 
     return (
         <div>
+            {loading ? <p>Loading...</p> : <img src={image}/>}
 
+            <button onClick={callImage}>New Image</button>
         </div>
     )
 }
